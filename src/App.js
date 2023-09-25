15a7c5b9e45efef59cce5eac91a7e4f6d98643eb
Luke Moore <Lukechristophermoore@gmail.com>
2023-09-25 12:47:50 +1000

    create api search function and map through results to display images, title and author of search results

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import getMangaBySearch from './utils/getMangaBySearch';
+
 import './App.css';
 
 function App() {
+
+  const [ searchTerm, setSearchTerm ] = useState('');
+  const [ searchResults, setSearchResults ] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getMangaBySearch(searchTerm)
+      .then((data) => {
+        setSearchResults(data);
+      })
+      .catch((error) => {
+        console.error('Error while fetching data:', error);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          name='search'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder='Enter manga title' 
+        />
+        <button type='submit'>Search</button>
+      </form>
+
+      <div className='image-container'>
+        {searchResults.map((manga, index) => (
+          <div>
+            <img key={index} src={manga.images.jpg.image_url} alt={`Manga ${index}`} />
+            <h2>{`${manga.title} by ${manga.authors[0].name}`}</h2>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/utils/getMangaBySearch.js
@@ -0,0 +1,22 @@
+const baseURL = 'https://api.jikan.moe/v4'
+    
+export default function getMangaBySearch(query) {
+    const endpoint = '/manga';
+    const url = `${baseURL}${endpoint}?q=${encodeURIComponent(query)}`;
+
+    return fetch(url)
+        .then((response) => {
+            if(!response.ok) {
+                throw new Error(`Network response was not ok: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data.data);
+            return data.data
+        })
+        .catch((error) => {
+            console.error('There was a problem with the fetch operation:', error);
+            throw error
+        });
+};
